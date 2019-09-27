16bcefbfbca600583ea2c345c42b2dada543d0b6
Ente <ducksource@duckpond.ch>
2019-09-27 20:23:02 +0200

    remove instances

--- src/app/model/counter.ts
+++ src/app/model/counter.ts
@@ -1,6 +1,5 @@
 import { autoserialize } from 'cerialize';
 
-import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { v4 as uuid } from 'uuid';
 
 export class Counter {
@@ -9,11 +8,6 @@ export class Counter {
   @autoserialize public count: number = 0;
   @autoserialize public plusCount: number = 1;
   @autoserialize public minusCount: number = -1;
-  public instances: Array<{
-      count: number;
-      timestamp: Date;
-      location: Geolocation;
-  }> = [];
 
   constructor() {
   }
