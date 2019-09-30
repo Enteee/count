b8ff6e925d9f2c6877a0ccd1cc27df6e59bb5b50
Ente <ducksource@duckpond.ch>
2019-09-30 06:04:27 +0000

    Not implement directive define action binding

--- src/app/not-implemented/not-implemented.directive.ts
+++ src/app/not-implemented/not-implemented.directive.ts
@@ -1,16 +1,27 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, Input, Renderer2, ElementRef, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 import { NotImplemented } from './not-implemented';
 
 @Directive({
   selector: '[appNotImplemented]',
 })
-export class NotImplementedDirective extends NotImplemented {
+export class NotImplementedDirective extends NotImplemented implements OnInit {
 
-  @HostListener('click')
-  @HostListener('ionSwipe')
-  async presentModal() {
-    super.presentModal();
+  @Input() public on = 'click';
+
+  constructor(public modalController: ModalController, private renderer: Renderer2, private elementRef: ElementRef) {
+    super(modalController);
+  }
+
+  ngOnInit() {
+    this.renderer.listen(
+      this.elementRef.nativeElement,
+      this.on,
+      e => {
+        this.presentModal();
+      }
+    );
   }
 
 }
