89a063df88c070e31610e485a3d7857b4311a18a
Ente <ducksource@duckpond.ch>
2019-10-01 12:16:10 +0200

    Counter settings with ReactiveFormsModule

--- src/app/counter-settings/counter-settings.module.ts
+++ src/app/counter-settings/counter-settings.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
@@ -17,7 +17,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     RouterModule.forChild(routes)
   ],
--- src/app/counter-settings/counter-settings.page.ts
+++ src/app/counter-settings/counter-settings.page.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+
+import { NavController } from '@ionic/angular';
 
 import { Counter } from '../model/counter';
+import { CounterService } from '../model/counter.service';
 
 @Component({
   selector: 'app-counter-settings',
@@ -11,13 +15,32 @@ import { Counter } from '../model/counter';
 export class CounterSettingsPage implements OnInit {
 
   private counter: Counter;
+  counterSettingsForm = new FormGroup({
+    title : new FormControl(''),
+  }); 
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private counterService: CounterService,
+    private navController: NavController
+  ) {}
 
   ngOnInit() {
-    if (this.route.snapshot.data['counter']) {
-      this.counter = this.route.snapshot.data['counter'];
+    this.counter = this.route.snapshot.data['counter'];
+    if (this.counter){
+      this.counterSettingsForm.patchValue(this.counter);
     }
+  }
+
+  async onSubmit() {
+    await this.counterService.save(
+      Object.assign(
+        this.counter,
+        this.counterSettingsForm.value
+      )
+    );
+    this.navController.pop();
   }
 }
 
