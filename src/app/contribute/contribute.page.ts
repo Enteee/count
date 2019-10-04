1c8d97fd1170f32322e75b386c2f3604b62f4668
Ente <ducksource@duckpond.ch>
2019-10-04 18:03:34 +0000

    contribute page with bug, feature, donations

--- src/app/contribute/contribute.page.ts
+++ src/app/contribute/contribute.page.ts
@@ -1,27 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 import { EmailComposer } from '@ionic-native/email-composer/ngx';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
+type TemplateType = 'bug' | 'feature';
 
 @Component({
   selector: 'app-contribute',
   templateUrl: './contribute.page.html',
   styleUrls: ['./contribute.page.scss'],
 })
-export class ContributePage implements OnInit {
+export class ContributePage {
 
   static readonly GITHUB_URL = 'https://github.com';
   static readonly PROJECT_NAME = 'Enteee/count';
-  static readonly TEMPLATE_ARGS = 'template=bug_report.md';
-  static readonly EMAIL_BODY = '/assets/email-templates/bug_report.md';
-  static readonly EMAIL = {
-    to: 'count-bug@duckpond.ch',
-    subject: 'New Issue',
-    body: `Please try to be as specific as possible.`
-  };
 
-  private email = ContributePage.EMAIL;
+  static readonly EMAIL_PREFIX = 'count-';
+  static readonly EMAIL_DOMAIN = 'duckpond.ch';
+  static readonly EMAIL_BODY_TEMPLATES = '/assets/email-templates/';
+
+  static readonly TEMPLATE_BUG = 'bug_report.md';
+  static readonly TEMPLATE_FEATURE = 'feature_request.md';
+
+  static readonly LIBREPAY_URL= 'https://liberapay.com';
+  static readonly LIBREPAY_USER = 'Ente';
 
   constructor(
     private httpClient: HttpClient,
@@ -30,32 +33,59 @@ export class ContributePage implements OnInit {
   ) {
   }
 
-  ngOnInit() {
-    this.httpClient.get(
-      ContributePage.EMAIL_BODY,
+  private getTemplateName(
+    templateType: TemplateType
+  ): string {
+    switch (templateType){
+      default:
+      case 'bug':
+        return ContributePage.TEMPLATE_BUG;
+      break;
+      case 'feature':
+        return ContributePage.TEMPLATE_FEATURE;
+      break;
+    }
+  }
+
+  private async getEmailTemplate(
+    templateType: TemplateType
+  ){
+    let templateName = this.getTemplateName(templateType);
+    let body = await this.httpClient.get(
+      `${ContributePage.EMAIL_BODY_TEMPLATES}/${templateName}`,
       {
         responseType: 'text'
       }
-    ).subscribe(
-      body => {
-        this.email = {
-          ...ContributePage.EMAIL,
-          ...{
-            body,
-          }
-        };
-      }
-    );
+    ).toPromise();
+
+    return {
+      to: `${ContributePage.EMAIL_PREFIX}${templateType}@${ContributePage.EMAIL_DOMAIN}`,
+      subject: `[${templateType}]: `,
+      body: body,
+    }
   }
 
-  openNewIssue() {
+  async openNewGithub(
+    templateType: TemplateType
+  ) {
+    let templateName = this.getTemplateName(templateType);
     this.iab.create(
-      `${ContributePage.GITHUB_URL}/${ContributePage.PROJECT_NAME}/issues/new?${ContributePage.TEMPLATE_ARGS}`
+      `${ContributePage.GITHUB_URL}/${ContributePage.PROJECT_NAME}/issues/new?template=${templateName}`
     );
   }
 
-  async openNewIssueViaMail() {
-    this.emailComposer.open(this.email);
+  async openNewMail(
+    templateType: TemplateType
+  ) {
+    this.emailComposer.open(
+      await this.getEmailTemplate(templateType)
+    );
+  }
+
+  async openDonate(){
+    this.iab.create(
+      `${ContributePage.LIBREPAY_URL}/${ContributePage.LIBREPAY_USER}`
+    );
   }
 
 }
