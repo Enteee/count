789becc1c1862d93c75584e44e15bcb3722a9383
Ente <ducksource@duckpond.ch>
2019-10-20 14:51:42 +0000

    help page test

--- src/app/help/help.page.spec.ts
+++ src/app/help/help.page.spec.ts
@@ -1,16 +1,36 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { Deploy } from 'cordova-plugin-ionic/dist/ngx';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
 import { HelpPage } from './help.page';
 
 describe('HelpPage', () => {
   let component: HelpPage;
   let fixture: ComponentFixture<HelpPage>;
+  let deploy: Deploy;
+  let iab: InAppBrowser;
 
   beforeEach(async(() => {
+
+    deploy = {
+      getConfiguration: () => {},
+    } as any;
+    spyOn(
+      deploy,
+      'getConfiguration'
+    );
+
+    iab = {} as InAppBrowser;
+
     TestBed.configureTestingModule({
       declarations: [ HelpPage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        { provide: Deploy, useValue: deploy },
+        { provide: InAppBrowser, useValue: iab },
+      ]
     })
     .compileComponents();
   }));
