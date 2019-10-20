b7a285eba6746f75dbe41a0f9b85a43f65d7558c
Enteee <ducksource@duckpond.ch>
2019-10-20 14:05:16 +0200

    Feature/initialize testing (#31)
    * Reviving some tests
    
    * fixing more tests
    
    * remove fuel preview
    
    * model serialize / deserialize tests
    
    * remove geolocation
    
    * services tests
    
    * enable tests
    
    * make counter-settings page work
    
    * minimal testing for counters and counters-settings
    
    * testing for not implemented modal page
    
    * Minimal testing for not implemented component
    
    * minimal testign for settings page
    
    * linting
    
    * run headless tests
    
    * Code coverage reports
    
    * Coveralls parallel
    
    * Coverage status button
    
    * fix workflow
    
    * Fixing tests after merge
    
    * linting

--- src/app/not-implemented/not-implemented.component.spec.ts
+++ src/app/not-implemented/not-implemented.component.spec.ts
@@ -1,16 +1,48 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Renderer2, ElementRef } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ModalController } from '@ionic/angular';
+
+import { AppState } from '../models/app-state';
+import { AppStateRepositoryService } from '../models/app-state-repository.service';
 
 import { NotImplementedComponent } from './not-implemented.component';
 
 describe('NotImplementedComponent', () => {
   let component: NotImplementedComponent;
   let fixture: ComponentFixture<NotImplementedComponent>;
+  let renderer: Renderer2;
+  let elementRef: ElementRef;
+  let modalController: ModalController;
+  let appStateRepositoryService: AppStateRepositoryService;
+  let appState: AppState;
 
   beforeEach(async(() => {
+
+    renderer = {} as any;
+
+    elementRef = {} as any;
+
+    modalController = {} as any;
+
+    appState = new AppState();
+    appStateRepositoryService = new AppStateRepositoryService(
+      {} as any
+    );
+
+    spyOnProperty(
+      appStateRepositoryService,
+      'state'
+    ).and.returnValue(appState);
+
     TestBed.configureTestingModule({
       declarations: [ NotImplementedComponent ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {provide: Renderer2, useValue: renderer},
+        {provide: ElementRef, useValue: elementRef},
+        {provide: ModalController, useValue: modalController},
+        {provide: AppStateRepositoryService, useValue: appStateRepositoryService},
+      ],
     })
     .compileComponents();
   }));
