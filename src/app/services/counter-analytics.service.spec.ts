7ebccadea782bb1a9a0f150cc95ef60aaeddce23
Ente <ducksource@duckpond.ch>
2020-03-01 12:32:24 +0000

    analytics add totals

--- src/app/services/counter-analytics.service.spec.ts
+++ src/app/services/counter-analytics.service.spec.ts
@@ -1,5 +1,5 @@
 import { Counter } from '../models/counter';
-import { CountEvent } from '../models/count-event';
+import { CountEvent, CountEventType } from '../models/count-event';
 import { CountEventRepositoryService } from '../models/count-event-repository.service';
 
 import { CounterAnalyticsService } from './counter-analytics.service';
@@ -91,5 +91,42 @@ describe('CounterAnalyticsService', () => {
       0,  // friday
       0,  // saturday
     ]);
+  });
+
+  it('can get totals', () => {
+
+    countEvents.push(new CountEvent(counter.id, 1));
+    countEvents[0].timestamp = new Date('Tue Oct 29 2019 19:41:25 GMT+0100 (CET)');
+
+    countEvents.push(new CountEvent(counter.id, 1));
+    countEvents[1].timestamp = new Date('Tue Oct 29 2019 19:41:25 GMT+0100 (CET)');
+
+    countEvents.push(new CountEvent(counter.id, -3));
+    countEvents[2].timestamp = new Date('Tue Oct 29 2019 19:41:25 GMT+0100 (CET)');
+
+    countEvents.push(new CountEvent(counter.id, 69));
+    countEvents[3].timestamp = new Date('Fri Nov 1 2019 11:11:11 GMT+0100 (CET)');
+
+    countEvents.push(new CountEvent(counter.id, 0, null, CountEventType.Reset));
+    countEvents[3].timestamp = new Date('Fri Nov 1 2019 12:12:12 GMT+0100 (CET)');
+
+    const result = service.extractTotalData(counter);
+
+    expect(countEventRepositoryService.getByCounter).toHaveBeenCalledTimes(1);
+    expect(countEventRepositoryService.getByCounter).toHaveBeenCalledWith(counter);
+    expect(result).toEqual({
+      totals: {
+        positive: 71,
+        negative: 3,
+        neutral: 0,
+      },
+      events: {
+        change: 4,
+        reset: 1,
+        positive: 3,
+        negative: 1,
+        neutral: 1,
+      }
+    });
   });
 });
--- src/app/services/counter-analytics.service.ts
+++ src/app/services/counter-analytics.service.ts
@@ -26,6 +26,21 @@ export type DateAggregationFunctions = (
   | 'getUTCSeconds'
 );
 
+export type TotalData = {
+  totals: {
+    positive: number;
+    negative: number;
+    neutral: number;
+  },
+  events: {
+    change: number;
+    reset: number;
+    positive: number;
+    negative: number;
+    neutral: number;
+  }
+}
+
 
 @Injectable({
   providedIn: 'root'
@@ -72,5 +87,57 @@ export class CounterAnalyticsService {
 
     return data;
   }
+  
+  public extractTotalData(
+    counter: Counter,
+    countEventFilterCallback: CountEventFilterCallback = () => true,
+  ): TotalData {
+    const countEvents = this.countEventRepository.getByCounter(counter);
+    const data = {
+      totals: {
+        positive: 0,
+        negative: 0,
+        neutral: 0,
+      },
+      events: {
+        change: 0,
+        reset: 0,
+        positive: 0,
+        negative: 0,
+        neutral: 0,
+      }
+    }
 
+    for (const countEvent of countEvents) {
+
+      if (!countEventFilterCallback(countEvent)) {
+        continue;
+      }
+
+      // aggregate counts
+      if (countEvent.delta > 0) {
+        data.events.positive += 1;
+        data.totals.positive += countEvent.delta;
+      } else if(countEvent.delta < 0) {
+        data.events.negative += 1;
+        data.totals.negative -= countEvent.delta;
+      } else {
+        data.events.neutral += 1;
+        data.totals.neutral += countEvent.delta; // should always  be 0
+      }
+
+      // aggregate types
+      switch(countEvent.type){
+        case "change":
+          data.events.change += 1;
+        break;
+        case "reset":
+          data.events.reset += 1;
+        break;
+      }
+
+    }
+
+    return data;
+  }
 }
