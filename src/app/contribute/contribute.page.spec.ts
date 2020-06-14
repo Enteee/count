112d65687e9fed0d5fc715656f7bbe21e851946f
Enteee <ducksource@duckpond.ch>
2020-06-14 13:56:32 +0200

    add option to disable direct monetization (#58)
    * add option to disable direct monetization
    
    * Upload artefact on release
    
    * apply patches step
    
    * Add store release patch
    
    * patches -> patch
    
    * patch first
    
    * print git diff after patching

--- src/app/contribute/contribute.page.spec.ts
+++ src/app/contribute/contribute.page.spec.ts
@@ -4,6 +4,10 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClient } from '@angular/common/http';
 import { EmailComposer } from '@ionic-native/email-composer/ngx';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
+import { AppState } from '../models/app-state';
+import { AppStateService } from '../services/app-state.service';
+
 import { ContributorService } from '../services/contributor.service';
 
 import { ContributePage } from './contribute.page';
@@ -15,6 +19,8 @@ describe('ContributePage', () => {
   let iab: InAppBrowser;
   let emailComposer: EmailComposer;
   let contributorService: ContributorService;
+  let appStateService: AppStateService;
+  let appState: AppState;
 
   beforeEach(async(() => {
 
@@ -28,6 +34,18 @@ describe('ContributePage', () => {
       'create',
     );
 
+    appState = new AppState();
+    appStateService = new AppStateService(
+      {} as any,
+      {} as any,
+      {} as any
+    );
+
+    spyOnProperty(
+      appStateService,
+      'appState'
+    ).and.returnValue(appState);
+
     emailComposer = {} as any;
 
     contributorService = new ContributorService();
@@ -39,6 +57,7 @@ describe('ContributePage', () => {
         { provide: HttpClient, useValue: httpClient },
         { provide: EmailComposer, useValue: emailComposer },
         { provide: InAppBrowser, useValue: iab },
+        { provide: AppStateService, useValue: appStateService },
         { provide: ContributorService, useValue: contributorService },
       ]
     })
