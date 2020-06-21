e7feb218ccaad49562f6361fff515935b62caa21
Ente <ducksource@duckpond.ch>
2020-06-21 23:33:28 +0200

    Create feature graphic

--- icon_update.sh
+++ icon_update.sh
@@ -5,15 +5,20 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 TOPLEVEL="$(git -C "${DIR}" rev-parse --show-toplevel)"
 
 IN_ICON="${1:-src/assets/icon.svg}"
+IN_FEATURE_GRAPHIC="${2:-src/assets/feature-graphic.svg}"
 
 OUT_FAVICON="src/assets/favicon.ico"
+
 OUT_ICON="resources/icon.png"
 OUT_ICON_PLAYSTORE="resources/icon-playstore.png"
 OUT_SPLASH="resources/splash.png"
 
+OUT_FEATURE_GRAPHIC="resources/feature-graphic.png"
+
 (
   cd "${TOPLEVEL}"
 
+  # Favicon
   convert \
     -density 384 \
     "${IN_ICON}" \
@@ -21,6 +26,7 @@ OUT_SPLASH="resources/splash.png"
     -define icon:auto-resize \
     "${OUT_FAVICON}"
 
+  # Icon
   convert \
     -density 1200 \
     -fill "#8c8c8cff" -opaque "#000000ff" \
@@ -40,6 +46,14 @@ OUT_SPLASH="resources/splash.png"
     "${OUT_ICON}" \
     -resize 512x512\! \
     "${OUT_ICON_PLAYSTORE}" \
+
+  # Feature Graphic
+  convert \
+    -density 1200 \
+    -fill "#8c8c8cff" -opaque "#000000ff" \
+    "${IN_FEATURE_GRAPHIC}" \
+    -resize 1024x500 \
+    "${OUT_FEATURE_GRAPHIC}"
 
   ionic cordova resources
 )
