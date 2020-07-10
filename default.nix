475ab597ab5d0d3ec35c1bacfe3c5bfcbd96a863
Ente <ducksource@duckpond.ch>
2020-07-10 13:43:06 +0000

    build tools

--- default.nix
+++ default.nix
@@ -73,22 +73,41 @@ in
       export PATH="''${PATH}:''${NPM_CONFIG_PREFIX}/bin"
       ${nodejs}/bin/npm set prefix "''${NPM_CONFIG_PREFIX}/.npm-global"
 
-      ${nodejs}/bin/npm install
-
-      ionic() { npx ionic "''$@"; }
+      ionic() { ${nodejs}/bin/npx ionic "''$@"; }
       export -f ionic
 
-      ng() { npx ng "''$@"; }
+      ng() { ${nodejs}/bin/npx ng "''$@"; }
       export -f ng
 
-      ncu() { npx ncu "''$@"; }
+      ncu() { ${nodejs}/bin/npx ncu "''$@"; }
       export -f ncu
+
+      build() {
+        STOREPASS="''${1?Missing Storepass first argument}" && shift
+        ${nodejs}/bin/npm install
+        ${nodejs}/bin/npx ionic cordova build android "''${@}"
+
+        export STOREPASS
+        find \
+          platforms/ \
+          -name '*.apk' \
+          -print0 \
+        | xargs -0 -n1 -I{} \
+          ./apk_finalize.sh \
+            {} \
+            "count.apk"
+      }
+      export -f build
+
+      buildRelease() {
+        STOREPASS="''${1?Missing Storepass first argument}" && shift
+        build "''${STOREPASS}" --prod --release "''${@}"
+      }
+      export -f buildRelease
+
 
       export ANDROID_SDK_ROOT="$PWD/.android-sdk"
       export ANDROID_HOME="$ANDROID_SDK_ROOT"
-
-      ncu
-
       exec ${fhsEnv}/bin/${drvName}-fhs-env
     '';
   }
